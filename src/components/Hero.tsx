c6024294288d5d71f4b9c8b0b3253848d351d225
SenseiTumelo <tmotjopi@gmail.com>
2026-07-31 14:32:25 +0200

    set data and get data implementation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,24 +4,29 @@ import { Button } from "./Button";
 export const Hero = () => {
  interface apiData{
   cityName: String,
-  temperature: Number
+  temperature: Number,
+  humidity: string,
+  feelsLike: string
 }
 
-  const [data, setData] = useState<apiData[]>([]);
+  const [data, setData] = useState();
        //const apiKey = "912533d7b0a5f9eaa2cb9be133558ccb";
         const apiKey = import.meta.env.VITE_API_KEY;
+   
       
-const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>)=>{
+const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>)=>{
       const cityName = e.target.city.value;
       e.preventDefault(); 
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+      await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
       .then(res=>res.json())
-      .then(data=>{ console.log(data); setData(data)});
+      .then(data=>{ console.log(data); setData(data); localStorage.setItem("weater-details",data.city.name)});
   
 } 
-if(!data){
+
+const getData = localStorage.getItem("weather-details");
+/*if(!data){
   return <div>Loading...</div>
-}
+}*/
  return (
     <>
          <main className="hero-section">
@@ -29,7 +34,11 @@ if(!data){
               <div className="card card-1">
                     
                    <div className="currentWeather">
-                    <h1></h1>
+                    <h1>
+                     {
+                      getData
+                     }
+                  </h1>
                    </div>
               </div>
               
