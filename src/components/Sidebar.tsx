cf6843cf4239aa08ef9761282523c7b83d49c459
SenseiTumelo <tmotjopi@gmail.com>
2026-08-06 10:05:32 +0200

    added navigation links, the links are stored in the data.ts file

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,22 @@
-import {Text} from './Text'
+import { NavLink } from 'react-router-dom';
+import {Text} from './Text';
+import {sidebarLinks} from '../data/appdata'
 
 export const Sidebar = () => {
   return (
         <div className="side-nav">
           <Text variant="h2">Weather Today</Text>  
+          <nav className="nav-list">
+            {sidebarLinks.map((link)=>
+            <NavLink to={link.path} id={link.id} className='navigator'>
+                <img src={link.icon} alt="" className="icons" />
+                {link.linkName}
+            </NavLink>
+            )}
+
+          </nav>
+          <div>
+          </div>
         </div>   
   )
 }
